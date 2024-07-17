1a4b18a83d48d6fbeeca317936ee0d653f09a0ee
i360cammy <cammypope96@gmail.com>
2024-07-17 12:05:26 +0100

    First Commit

---
+++ game.py
@@ -0,0 +1,94 @@
+import pygame
+import sys
+from scripts.entities import PhysicsEntity, Player
+from scripts.utils import load_image, load_images, Animation
+from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
+
+# This is the main game class. It contains all the game logic.
+class Game:
+    def __init__(self):
+    
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((640,480))
+
+        pygame.display.set_caption("Ninja Game")
+
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False]
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_duration=6),
+            'player/run': Animation(load_images('entities/player/run'), img_duration=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
+        }
+
+        self.player = Player(self, (50,50), (8,15))
+
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        self.tilemap.load('map.json')
+
+        self.scroll = [0, 0]
+
+        self.clouds = Clouds(self.assets['clouds'], count=16)
+
+    # This is the main game loop.
+    def run(self):
+        while True:
+
+            self.display.blit(self.assets['background'], (0,0))
+
+            self.scroll[0] += (self.player.rect().centerx -  self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery -  self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display, offset=render_scroll)
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                        self.player.jump()
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
+
+            # This is the code that makes the game run at 60 frames per second.
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
